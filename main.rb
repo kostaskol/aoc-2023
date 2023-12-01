73826c3bca7a3a02a7749ec72e9f96eb173f2b8a
Kostas Kolivas <kkolivas@skroutz.gr>
2023-12-01 11:47:33 +0200

    Parametrize script

--- main.rb
+++ main.rb
@@ -1,4 +1,23 @@
-require './days/d1'
-require './utils'
+# frozen_string_literal: true
 
-puts Days::D1.solve(2, load(1, test: false))
+require 'optparse'
+require 'date'
+require_relative 'days/d1'
+require_relative 'utils'
+
+opts = {
+  part: 1,
+  day: Date.today.day
+}
+
+OptionParser.new do |opt|
+  opt.on('--part PART', '-p PART') { |o| opts[:part] = o.to_i }
+  opt.on('--day DAY', '-d DAY') { |o| opts[:day] = o.to_i }
+end.parse!
+
+raise 'Day not released yet!' if Date.new(Date.today.year, Date.today.month, opts[:day]) > Date.today
+raise 'Part must be 1 or 2' if opts[:part] != 1 && opts[:part] != 2
+
+day_class = Object.const_get("Days::D#{opts[:day]}")
+
+puts day_class.solve(opts[:part], Utils.load(opts[:day], test: false))
--- utils.rb
+++ utils.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-def load(day, test: false)
-  filename = "input/d#{day}#{test ? '_test' : ''}.txt"
-  File.readlines(filename).map(&:strip)
+module Utils
+  def self.load(day, test: false)
+    filename = "input/d#{day}#{test ? '_test' : ''}.txt"
+    File.readlines(filename).map(&:strip)
+  end
 end
